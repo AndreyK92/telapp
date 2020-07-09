db16fc3f1d7307fa7a41f3598e73958f36d537c4
Andrey Kalashnikov <andrey@ifoundagent.com>
2020-07-09 11:30:01 -0700

    Adding babelrc

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div class="guth">Guth And Andrey</div>,
+  document.getElementById('app')
+);
+
+module.hot.accept();
