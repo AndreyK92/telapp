cf2bf40b76c98723c30f4999422eca6324c6a8ab
Andrey Kalashnikov <andrey@ifoundagent.com>
2020-07-09 11:19:45 -0700

    Adding config files

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const publicContentPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: './src/index.js',
+  module: {
+    rules: [
+    {
+      test: /\.js|jsx$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  output: {
+    path: publicContentPath,
+    publicPath: '/',
+    filename: 'app.js'    
+  },
+  plugins: [ new webpack.HotModuleReplacementPlugin() ],
+  devServer: {
+    contentBase: publicContentPath,
+    hot: true
+  } 
+};
